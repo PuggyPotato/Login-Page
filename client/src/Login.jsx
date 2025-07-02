bdeed7146f511ed932dac94988d07f8ec3948617
Puggy Potato <qingyaurou@gmail.com>
2025-07-02 19:52:04 +0800

    Replicated Styling on register page

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,11 +1,18 @@
-
+import { useNavigate } from "react-router-dom"
 
 
 function Login(){
+    const navigate = useNavigate();
 
+    function navigateToRegister(){
+        navigate("./Register")
+    }
     return(
         <>
             <div id="container">
+                <div id="title">
+                    <h1>Login Page</h1>
+                </div>
                 <div id="credentials">
                     <div id="usernameContainer">
                         <label>Username:</label><input id="usernameInput"/>
@@ -15,11 +22,11 @@ function Login(){
                     </div>
                 </div>
                 <div id="auth">
-                    <div id="loginButton">
-                        <button>Login</button>
+                    <div id="registerButtonLogin">
+                        <button onClick={navigateToRegister}>Register</button>
                     </div>
-                    <div id="registerButton">
-                        <button>Register</button>
+                    <div id="loginButtonLogin">
+                        <button>Login</button>
                     </div>
                 </div>
             </div>
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,15 +1,38 @@
+import { useNavigate } from "react-router-dom"
 
 
 
 
 
 function Register(){
+    const navigate = useNavigate();
 
+    function navigateToLogin(){
+        navigate("/")
+    }
 
     return(
-        <>
-            <div>
-
+        <>    
+            <div id="container">
+                <div id="title">
+                    <h1>Register Page</h1>
+                </div>
+                <div id="credentials">
+                    <div id="usernameContainer">
+                        <label>Username:</label><input id="usernameInput"/>
+                    </div>
+                    <div id="passwordContainer">
+                        <label>Password:</label><input id="passwordInput"/>
+                    </div>
+                </div>
+                <div id="auth">
+                    <div id="loginButtonRegister">
+                        <button onClick={navigateToLogin}>Login</button>
+                    </div>
+                    <div id="registerButtonRegister">
+                        <button>Register</button>
+                    </div>
+                </div>
             </div>
         </>
     )
