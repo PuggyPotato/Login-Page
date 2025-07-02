b9bda72efceca366b1c522919ab181d778e475f7
Puggy Potato <qingyaurou@gmail.com>
2025-07-02 19:45:23 +0800

    Added register button

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -8,14 +8,19 @@ function Login(){
             <div id="container">
                 <div id="credentials">
                     <div id="usernameContainer">
-                        <label>Username:</label><input/>
+                        <label>Username:</label><input id="usernameInput"/>
                     </div>
                     <div id="passwordContainer">
-                        <label>Password:</label><input/>
+                        <label>Password:</label><input id="passwordInput"/>
                     </div>
                 </div>
-                <div id="loginButton">
-                    <button>Login</button>
+                <div id="auth">
+                    <div id="loginButton">
+                        <button>Login</button>
+                    </div>
+                    <div id="registerButton">
+                        <button>Register</button>
+                    </div>
                 </div>
             </div>
         </>
